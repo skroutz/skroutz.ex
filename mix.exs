7bd392d0f670ba65dcbbfb754a522e97d62bbf16
Dimitris Zorbas <zorbash@skroutz.gr>
2016-02-21 20:07:37 +0200

    Add package settings in mix.exs

--- mix.exs
+++ mix.exs
@@ -7,28 +7,32 @@ defmodule Skroutz.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,
+     source_url: "https://github.com/skroutz/skroutz.ex",
+     description: description]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.8"},
      {:poison, "~> 2.0"},
+
+     # Test dependencies
      {:exvcr, "~> 0.7", only: :test}]
+  end
+
+  defp description, do: "Elixir library for accessing the Skroutz API"
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "*.md"],
+      maintainers: ["Dimitris Zorbas"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/skroutz/skroutz.ex"}
+    ]
   end
 end
