03e9ef42a258cad2a70c8ef31e8b953f26bea29a
Dimitrios Zorbas <zorbash@hotmail.com>
2016-02-21 21:27:34 +0200

    Fix indentation in oauth2_test.exs

--- test/skroutz/oauth2_test.exs
+++ test/skroutz/oauth2_test.exs
@@ -19,7 +19,7 @@ defmodule Skroutz.Oauth2Test do
 
   test "issue_public_token, when credentials are invalid" do
     {:ok, skroutz} = Skroutz.new(%{client_id: "invalid_id",
-                                     client_secret: "invalid_secret"})
+                                   client_secret: "invalid_secret"})
 
     use_cassette "public_token_error" do
       assert (skroutz |> Skroutz.Oauth2.issue_public_token)
